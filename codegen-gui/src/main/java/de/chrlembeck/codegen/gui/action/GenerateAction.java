5f05e38d5a0a89b856fc5dedc608f04285d1a0ff
Christoph Lembeck <chrlembeck@gmail.com>
2017-09-05 20:50:46 +0200

    Generate action is now feature complete

--- codegen-gui/src/main/java/de/chrlembeck/codegen/gui/action/GenerateAction.java
+++ codegen-gui/src/main/java/de/chrlembeck/codegen/gui/action/GenerateAction.java
@@ -3,6 +3,7 @@ package de.chrlembeck.codegen.gui.action;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -19,12 +20,15 @@ import de.chrlembeck.codegen.generator.SimpleTemplateResolver;
 import de.chrlembeck.codegen.generator.lang.TemplateFile;
 import de.chrlembeck.codegen.generator.lang.TemplateStatement;
 import de.chrlembeck.codegen.generator.output.BasicOutputPreferences;
-import de.chrlembeck.codegen.generator.output.BufferedOutput;
+import de.chrlembeck.codegen.generator.output.FileOutput;
+import de.chrlembeck.codegen.generator.output.OverwritePreferences;
 import de.chrlembeck.codegen.gui.CodeGenGui;
 import de.chrlembeck.codegen.gui.IconFactory;
 import de.chrlembeck.codegen.gui.TabComponent;
 import de.chrlembeck.codegen.gui.TemplatePanel;
-import de.chrlembeck.codegen.gui.util.ToStringWrapper;
+import de.chrlembeck.codegen.gui.UserSettings;
+import de.chrlembeck.codegen.gui.dialog.GenerateDialog;
+import de.chrlembeck.codegen.gui.dialog.GenerateDialog.OutputSelection;
 
 /**
  * Action zum Starten des Code-Generators.
@@ -75,59 +79,118 @@ public class GenerateAction extends AbstractAction {
         }
         final TemplatePanel templatePanel = getSelectedTemplatePanel();
         if (templatePanel == null) {
-            JOptionPane.showMessageDialog(codeGenGui, "Es ist kein Template ausgewählt.", "Kein Template",
-                    JOptionPane.ERROR_MESSAGE);
+            codeGenGui.showErrorMessage("Es ist keine Template-Datei ausgewählt.", "Keine Template-Datei");
             return;
         }
-
         templatePanel.validateTemplate();
         if (templatePanel.hasErrors()) {
-            JOptionPane.showMessageDialog(codeGenGui, "Bitte erst die Fehler beheben.");
+            codeGenGui.showErrorMessage("Bitte erst die Fehler beheben.", "Fehler in Templates");
+            return;
+        }
+        if (templatePanel.isNewArtifact()) {
+            codeGenGui.showErrorMessage(
+                    "Vor dem Generieren muss die Template-Datei mindestens einmal gespeichert worden sein.",
+                    "Bitte erst speichern");
+            return;
+        }
+        final TemplateFile templateFile = templatePanel.getTemplateFile();
+        final List<TemplateStatement> templates = templateFile.getTemplateStatements();
+        if (templates == null || templates.isEmpty()) {
+            codeGenGui.showErrorMessage("Die Template-Date enthält keine Template-Definitionen.", "Keine Templates");
             return;
         }
 
-        final TemplateFile templateFile = templatePanel.getTemplateFile();
+        final GenerateDialog generateDialog = new GenerateDialog(codeGenGui, templates, TemplateStatement::getName);
+        generateDialog.setVisible(true);
+        if (generateDialog.getResult() == GenerateDialog.RESULT_OK) {
+            final OutputSelection outputSelection = generateDialog.getOutputSelection();
+            Generator generator;
+            switch (outputSelection) {
+                case FILE_OUTPUT:
+                    generator = generateToFile(templateFile, generateDialog.getOutputDirectory(),
+                            generateDialog.getOverwritePreferences());
+                    new UserSettings().setLastOutputDirectory(generateDialog.getOutputDirectory());
+                    break;
+                case APPLICATION_OUTPUT:
+                    generator = generateToGui(templateFile);
+                    break;
+                default:
+                    throw new IllegalStateException();
 
-        final List<TemplateStatement> templates = templateFile.getTemplateStatements();
-        @SuppressWarnings("unchecked")
-        final ToStringWrapper<TemplateStatement>[] templateWrappers = templates.stream()
-                .map(templateSt -> new ToStringWrapper<>(templateSt, TemplateStatement::getName))
-                .toArray(ToStringWrapper[]::new);
-        @SuppressWarnings("unchecked")
-        final ToStringWrapper<TemplateStatement> selection = (ToStringWrapper<TemplateStatement>) JOptionPane
-                .showInputDialog(codeGenGui,
-                        "Bitte ein Template auswälen.", "Template wählen", JOptionPane.QUESTION_MESSAGE, null,
-                        templateWrappers,
-                        templateWrappers[0]);
-        if (selection != null) {
-            final BufferedOutput out = new BufferedOutput();
-            final Path templatePath = templatePanel.getPath();
-            final URI templateResourceLocator = templatePath.toUri();
-            final SimpleTemplateResolver templateResolver = new SimpleTemplateResolver(templateResourceLocator);
-            final Generator generator = new Generator(templateResolver, out,
-                    new BasicOutputPreferences());
-            try {
-                generator.generate(templateResourceLocator, selection.getObject().getName(), getModel());
-                for (final String channel : out.getChannelNames()) {
-                    System.out.println("*** " + channel + "***");
-                    System.out.println(out.getContent(channel));
-                    System.out.println("*** end of " + channel + "***");
-                }
-            } catch (final IOException e) {
-                LOGGER.error("IO-Fehler bei der Generierung: " + e.getMessage(), e);
-                JOptionPane.showMessageDialog(codeGenGui, e.getMessage(), "IO-Fehler", JOptionPane.ERROR_MESSAGE);
-            } catch (final GeneratorException ge) {
-                // TODO: Call-Stack mit ausgeben
-                JOptionPane.showMessageDialog(codeGenGui,
-                        ge.getMessage() + "\n" + ge.getStartPosition().toShortString(),
-                        "Generator-Fehler", JOptionPane.ERROR_MESSAGE);
-                LOGGER.info(ge.getLocalizedMessage() + ": " + ge.printGeneratorStack(), ge);
-            } catch (final ParserException pe) {
-                JOptionPane.showMessageDialog(codeGenGui,
-                        pe.getMessage() + "\n" + pe.getStartPosition().toShortString(),
-                        "Parser-Fehler", JOptionPane.ERROR_MESSAGE);
             }
+            final TemplateStatement selectedTemplate = generateDialog.getSelectedTemplate();
+            generate(generator, templateFile, selectedTemplate.getName());
         }
+    }
+
+    /**
+     * Führt die eigentliche Generierung der Artefakte durch.
+     * 
+     * @param generator
+     *            Generator, der die Generierung vornehmen soll.
+     * @param templateFile
+     *            Template-Datei, aus der das Template gelesen werden soll.
+     * @param templateName
+     *            Name des Templates, welches angewandt werden soll.
+     */
+    private void generate(final Generator generator, final TemplateFile templateFile, final String templateName) {
+        try {
+            generator.generate(templateFile, templateName, getModel());
+        } catch (final IOException e) {
+            LOGGER.error("IO-Fehler bei der Generierung: " + e.getMessage(), e);
+            JOptionPane.showMessageDialog(codeGenGui, e.getMessage(), "IO-Fehler", JOptionPane.ERROR_MESSAGE);
+        } catch (final GeneratorException ge) {
+            // TODO: Call-Stack mit ausgeben
+            JOptionPane.showMessageDialog(codeGenGui,
+                    ge.getMessage() + "\n" + ge.getStartPosition().toShortString(),
+                    "Generator-Fehler", JOptionPane.ERROR_MESSAGE);
+            LOGGER.info(ge.getLocalizedMessage() + ": " + ge.printGeneratorStack(), ge);
+        } catch (final ParserException pe) {
+            JOptionPane.showMessageDialog(codeGenGui,
+                    pe.getMessage() + "\n" + pe.getStartPosition().toShortString(),
+                    "Parser-Fehler", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    /**
+     * Konfiguriert den Generator für die Ausgabe der Artefakte in das Dateisystem.
+     * 
+     * @param templateFile
+     *            TemplateFile, aus dem die Templates gelesen werden sollen.
+     * @param outputDirectory
+     *            Verzeichnis, in das die Ausgabe erfolgen soll.
+     * @param overwritePreferences
+     *            Verhalten des Generators beim Überschreiben existierender Dateien.
+     * @return Konfiguriertwer Generator für die Ausgabe in das Dateisystem.
+     */
+    private Generator generateToFile(final TemplateFile templateFile,
+            final Path outputDirectory, final OverwritePreferences overwritePreferences) {
+        final FileOutput out = new FileOutput(outputDirectory);
+        final SimpleTemplateResolver templateResolver = new SimpleTemplateResolver(templateFile);
+        final BasicOutputPreferences outputPreferences = new BasicOutputPreferences();
+        outputPreferences.setDefaultCharset(Charset.forName("UTF-8"));
+        outputPreferences.setDefaultOverwritePreferences(overwritePreferences);
+        final Generator generator = new Generator(templateResolver, out,
+                outputPreferences);
+        return generator;
+    }
+
+    /**
+     * Konfiguriert einen Generator für die Ausgabe der Artefakte in die grafische Oberfläche.
+     * 
+     * @param templateFile
+     *            Template-Datei, aus der die Templates gelesen werden sollen.
+     * @return Konfiurierter Generator für die Ausgabe in die GUI.
+     */
+    private Generator generateToGui(final TemplateFile templateFile) {
+        final GuiOutput out = new GuiOutput(codeGenGui);
+        final URI templateResourceLocator = templateFile.getResourceIdentifier();
+        LOGGER.debug("templateResourceLocator=" + templateResourceLocator);
+        final SimpleTemplateResolver templateResolver = new SimpleTemplateResolver(templateFile);
+        final Generator generator = new Generator(templateResolver, out,
+                new BasicOutputPreferences());
+        codeGenGui.removeAllOutputPanels();
+        return generator;
     }
 
     /**
